a69ceed5f194aad42b60f834a5861ed83ac309d5
Beridze0 <beridzedavit904@gmail.com>
2025-09-09 21:27:52 +0400

    fixed sidebar curved borders

--- src/layout/sidebar/components/SidebarItem.jsx
+++ src/layout/sidebar/components/SidebarItem.jsx
@@ -14,10 +14,28 @@ export default function SidebarItem({
       to={link}
       onClick={onClick}
       className={({ isActive }) =>
-        `flex items-center gap-2 p-4 rounded-l-4xl 
-      rounded-r-2xl hover:bg-white hover:text-black duration-300 ${
-        isActive ? "bg-white text-black rounded-r-none" : ""
-      }`
+        `flex items-center gap-2 p-4 rounded-l-4xl relative 
+         rounded-r-2xl border-none 
+         
+         ${
+           isActive
+             ? `bg-gray-50 text-black rounded-r-none hover:text-black
+                before:opacity-100 after:opacity-100`
+             : `before:opacity-0 after:opacity-0 hover:text-white`
+         }
+      
+         before:content-[""] 
+         before:bg-transparent
+         before:absolute before:right-[0px] before:top-[-16px] 
+         before:w-4 before:h-4 before:rounded-full 
+         before:shadow-[3px_3px_0_-1px_#f9fafb]
+      
+         after:content-[""]
+         after:bg-transparent
+         after:absolute after:right-[0px] after:bottom-[-16px]
+         after:w-4 after:h-4 after:rounded-full
+         after:shadow-[3px_-3px_0_-1px_#f9fafb]
+        `
       }
     >
       <Icon size={22} />
--- src/layout/sidebar/components/SidebarNav.jsx
+++ src/layout/sidebar/components/SidebarNav.jsx
@@ -4,7 +4,7 @@ import { sidebarItems } from "../sidebarData";
 
 export default function SidebarNav() {
   return (
-    <ul className="flex-1 flex flex-col mt-6  pl-3">
+    <ul className="flex-1 text-neutral-400 flex flex-col mt-6  pl-3">
       {sidebarItems.map((item) => (
         <li key={item.label}>
           <SidebarItem label={item.label} icon={item.icon} link={item.link} />
