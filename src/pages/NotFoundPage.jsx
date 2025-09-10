0d0f4afa659919efbbadb55a316cdc52f5c57ff3
Beridze0 <beridzedavit904@gmail.com>
2025-09-10 18:59:54 +0400

    notfound fix

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -7,7 +7,7 @@ export default function NotFoundPage() {
       <h1 className="text-6xl font-bold text-gray-800">404</h1>
       <h2 className="text-xl font-semibold text-gray-600">Page Not Found</h2>
       <span className="text-sm mt-3 text-gray-500">
-        <NavLink to="/comparison" className="text-blue-700 hover:underline">
+        <NavLink to="/textcompare" className="text-blue-700 hover:underline">
           Click here
         </NavLink>{" "}
         to go back to the Comparison page
